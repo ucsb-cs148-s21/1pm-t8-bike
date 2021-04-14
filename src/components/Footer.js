b769f90500688583da4541a9945bea73bb868376
Anika Arora <anika_arora@ucsb.edu>
2021-04-13 17:44:54 -0700

    aa - edited starter code for Hello, World web app

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,7 @@ import Container from "react-bootstrap/Container";
 export default function Footer() {
   return (
     <Container>
-      App developed for CS148 S21. Check out the source code{" "}
-      <a href="https://github.com/vincentktieu101/ReactGoogleAuthStarter">here</a>
-      .
-      <br />
-      <br />
+      Based on starter code provided by Vincent Tieu. 
     </Container>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,22 +10,7 @@ export default function Home() {
   return (
     <Layout user={user}>
       <Container>
-        <h1>Hello! This is the Home page!</h1>
-        <div style={textStyle}>
-          This project was built in React, specically in Create React App
-          (the officially supported version of React developed by Facebook).
-          This starter code will focus on setting up Google Authentication and 
-          demonstrating examples of public and private pages. Notice that the
-          {" "}<a href="/profile">Profile</a> page is not located in
-          the NavBar until you login.
-        </div>
-        <br />
-        <div style={textStyle}>
-          This Home page is a public page, meaning that users can view 
-          this page without logging in. The Profile page is a private 
-          page, meaning that users have to login.
-        </div>
-        <br />
+        <h1>Hello, World.</h1>
       </Container>
     </Layout>
   );
