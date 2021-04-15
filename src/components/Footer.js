3031f8d179de683560824be20af9e58e8f171509
Rukmini Bapat <mini.bapat@gmail.com>
2021-04-14 22:52:15 -0700

    added credits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 export default function Footer() {
   return (
     <Container>
-      App developed for CS148 S21. Check out the source code{" "}
+      Based on React Starter Code from Vincent Tieu, developed for CS148 S21. Check out the source code{" "}
       <a href="https://github.com/vincentktieu101/ReactGoogleAuthStarter">here</a>
       .
       <br />
