b9037d068d198f5749892e68eb478e4e1016dff0
Rukmini Bapat <mini.bapat@gmail.com>
2021-04-15 22:14:52 -0700

    rb - update hello world message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout user={user}>
       <Container>
-        <h1>Hello World!</h1>
+        <h1>Hello, World!</h1>
         <br />
         <br />
       </Container>
