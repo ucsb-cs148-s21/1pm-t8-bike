5fe3167bfe8b41104a6295124278f94871ae4a55
AnikaArora <60118889+AnikaArora@users.noreply.github.com>
2021-04-21 23:25:10 -0700

    aa - update Map.js

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -2,29 +2,6 @@ import Layout from "../components/Layout";
 import Container from "react-bootstrap/Container";
 import getUser from "../utils/get-user";
 
-const textStyle = {maxWidth: "100%", width: "700px"}
-
-export default function Map() {
-  const user = getUser();
-
-  return (
-    <Layout user={user}>
-      <Container>
-        <h1>Welcome to Gaucho Bike Map!</h1>
-        <br />
-        <div style={textStyle}>
-          hello world!
-        </div>
-        <br />
-        <div style={textStyle}>
-          goodbye world!
-        </div>
-        <br />
-      </Container>
-    </Layout>
-  );
-}
-
 let map;
 
 function initMap() {
