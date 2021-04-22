d031a25b03f2aecaf7b1733e6ba7c7ddc8d42094
AnikaArora <60118889+AnikaArora@users.noreply.github.com>
2021-04-21 23:18:55 -0700

    aa - update Map.js

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -24,3 +24,12 @@ export default function Map() {
     </Layout>
   );
 }
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
