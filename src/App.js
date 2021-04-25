dd3470bf36a476b09cfaf6c957743e830b1d0e38
Rukmini Bapat <mini.bapat@gmail.com>
2021-04-24 23:07:31 -0700

    rb - map of ucsb starter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ import Map from "./pages/Map";
 import Profile from "./pages/Profile";
 import Private from "./pages/Private";
 import PageNotFound from "./pages/PageNotFound";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+function MyMap() {
+  
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 34.4134, lng: -119.8433 }}
+      //defaultOptions={{ styles: mapStyles }}
+    >
+      
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(MyMap));
 
 export default function App() {
   const [isSignedIn, setIsSignedIn] = useState(null);
@@ -54,6 +77,16 @@ export default function App() {
         <PrivateRoute exact path="/profile" Component={Profile} />
         <Route path="/" render={PageNotFound} />
       </Switch>
+      <div style={{ width: "50vw", height: "50vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
+          process.env.REACT_APP_GOOGLE_KEY
+        }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
     </BrowserRouter>
   );
 }
