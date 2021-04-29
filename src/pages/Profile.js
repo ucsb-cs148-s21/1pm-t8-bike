4e12ccdc7a92e214c24c6f46c59051466564c328
Calvin Dougher <cwdougher@umail.ucsb.edu>
2021-04-29 16:35:32 -0700

    cc - Profile Bug

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -13,14 +13,9 @@ export default function Profile() {
       <Container>
         <h1>{user.fullName}</h1>
         <img src={user.imageUrl} style={profileStyle} alt="profilePic" />
-<<<<<<< HEAD
         <div>
           Email: {user.email}
         </div>
-=======
-        <div>Email: {user.email}</div>
-        <br />
->>>>>>> 54d77b3d9a9336dc3868c69135ac1ebcb1bc2460
         <div>
           Bio: I'm a sophomore CS student who needs help navigating the bike
           paths!
