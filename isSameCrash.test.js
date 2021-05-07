01e0e2977275a2bd2bd3445b5ed115837979d4a2
Yvonne Liu <yvonneliu@umail.ucsb.edu>
2021-05-07 13:22:43 -0700

    added jestTest

---
+++ isSameCrash.test.js
@@ -0,0 +1,17 @@
+
+//import React from 'react';
+import { isSameCrash } from "./src/pages/Map";
+
+// test('two coords are same crash', () => {
+//   expect(isSameCrash(34.41359, -119.84634,34.41342,-119.84635)).toBe(true);
+// });
+
+describe("src/pages/", () => {
+  describe("isSameCrash", () => {
+    it("same return true, when coords within 200ft", () => {
+      expect(isSameCrash(34.41359, -119.84634,34.41342,-119.84635)).toBe(
+        true
+      );
+    });
+  });
+});
